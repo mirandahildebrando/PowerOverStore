461779d14a09745ee76a1772e44fa931033e4dba
mirandahildebrando <mirandahildebrando5@gmail.com>
2025-07-09 17:06:41 -0300

    first commit

---
+++ PowerOver/src/main/java/com/PowerOverStore/PowerOver/PowerOverApplication.java
@@ -0,0 +1,13 @@
+package com.PowerOverStore.PowerOver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerOverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerOverApplication.class, args);
+	}
+
+}
---
+++ PowerOver/src/test/java/com/PowerOverStore/PowerOver/PowerOverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PowerOverStore.PowerOver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerOverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
